553d71a2917510a822ef3348d73cc5f0b4928d57
Orion Dobos <oriondobos@gmail.com>
2024-06-08 11:46:37 -0700

    adding icon & string table resources, updating winmain.cpp

--- OrionEngine/BlankProject/Source/pch.h
+++ OrionEngine/BlankProject/Source/pch.h
@@ -1,6 +1,10 @@
+/* Precompiled Header file, accessible by everything in this game engine */
+
 #pragma once
 
 #include <windows.h>
+
+#include "../resource.h"
 
 #define MAX_NAME_STRING 256
 #define HInstance() GetModuleHandle(NULL)  // hInstance callback
